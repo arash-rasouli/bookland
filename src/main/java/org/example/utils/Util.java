6307c2b6618940694e9c9ac2f35533c158da896d
arash-rasouli <arash3908@gmail.com>
2023-05-07 02:43:43 +0330

    Add secrets.properties

--- src/main/java/org/example/utils/Util.java
+++ src/main/java/org/example/utils/Util.java
@@ -1,8 +1,14 @@
 package org.example.utils;
 
+import io.jsonwebtoken.*;
+
+import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
 public class Util {
     public static byte[] getSHA(String input){
@@ -16,13 +22,46 @@ public class Util {
         }
     }
 
-    public static String hexToString(byte[] hash)
-    {
+    public static String hexToString(byte[] hash){
         BigInteger number = new BigInteger(1, hash);
         StringBuilder hexString = new StringBuilder(number.toString(16));
         while (hexString.length() < 64){
             hexString.insert(0, '0');
         }
         return hexString.toString();
+    }
+
+    public static String createJWT(String userEmail, String key){
+        SignatureAlgorithm alg = SignatureAlgorithm.HS256;
+        SecretKeySpec signKey = new SecretKeySpec(Util.getSHA(key), alg.getJcaName());
+
+        Instant now = Instant.now();
+
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .claim("userEmail", userEmail)
+                .setId(UUID.randomUUID().toString())
+                .setIssuer("BOOKLAND")
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plus(1L, ChronoUnit.HOURS)))
+                .signWith(signKey, alg);
+
+        return jwtBuilder.compact();
+    }
+
+    public static Claims decodeJWT(String token, String key){
+        SignatureAlgorithm alg = SignatureAlgorithm.HS256;
+        SecretKeySpec signKey = new SecretKeySpec(Util.getSHA(key), alg.getJcaName());
+
+        try {
+            return Jwts.parserBuilder().setSigningKey(signKey).build().parseClaimsJws(token).getBody();
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public static boolean validateJwt(Claims claims){
+        if(!claims.getIssuer().equals("BOOKLAND")) return false;
+        if(claims.getExpiration().before(Date.from(Instant.now()))) return false;
+        return true;
     }
 }
