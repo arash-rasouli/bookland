ba5ded09ec2db79d499b68bb33f1e15935701df5
arash-rasouli <arash3908@gmail.com>
2023-05-07 11:40:32 +0330

    Add ResponseInfo as a dto

---
+++ src/main/java/org/example/dto/ResponseInfo.java
@@ -0,0 +1,56 @@
+package org.example.dto;
+
+import java.util.ArrayList;
+
+public class ResponseInfo {
+    private Object value;
+    private ArrayList<String> errors;
+    private String message;
+    private boolean success;
+
+    public ResponseInfo() {
+        value = null;
+        message = null;
+        success = true;
+        errors = new ArrayList<>();
+    }
+
+    public ResponseInfo(Object _value, boolean _success) {
+        value = _value;
+        message = "No Message";
+        errors = new ArrayList<>();
+        success = _success;
+    }
+
+    public ResponseInfo(Object _value, boolean _success, String _message) {
+        value = _value;
+        message = _message.isBlank() ? "No Message" : _message;
+        errors = new ArrayList<>();
+        success = _success;
+    }
+
+    public ResponseInfo(Object _value, boolean _success, String _message, ArrayList<String> _errors) {
+        value = _value;
+        message = _message.isBlank() ? "No Message" : _message;
+        errors = _errors;
+        success = _success;
+    }
+
+    public void addError(String error) {
+        errors.add(error);
+    }
+
+    public void addErrors(ArrayList<String> _errors) {
+        errors.addAll(_errors);
+    }
+
+    public String getMessage() { return message; }
+    public ArrayList<String> getErrors() { return errors; }
+    public Object getValue() { return value; }
+    public boolean getSuccess() { return success; }
+
+    public void setSuccess(boolean _success) { success = _success; }
+    public void setMessage(String _message) { message = _message; }
+    public void setErrors(ArrayList<String> _errors) { errors = _errors; }
+    public void setValue(Object _value) { value = _value; }
+}
