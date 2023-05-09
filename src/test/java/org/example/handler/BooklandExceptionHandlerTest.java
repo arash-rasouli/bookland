17aa1eb02efdecbdeeccab8c4f482453fb8457d4
arash-rasouli <arash3908@gmail.com>
2023-05-10 02:08:42 +0330

    Add unit test for BooklandExceptionHandler

--- src/test/java/org/example/handler/BooklandExceptionHandlerTest.java
+++ src/test/java/org/example/handler/BooklandExceptionHandlerTest.java
@@ -1,5 +1,9 @@
 package org.example.handler;
 
+import org.example.dto.ResponseInfo;
+import org.example.exceptions.AlreadyExist;
+import org.example.exceptions.NotFound;
+import org.example.exceptions.NotMatch;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,11 +24,13 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BooklandExceptionHandlerTest {
-    MethodArgumentNotValidException ex;
+    MethodArgumentNotValidException methodArgumentNotValidException;
+    HttpMessageNotReadableException httpMessageNotReadableException;
+
     BooklandExceptionHandler handler = new BooklandExceptionHandler();
 
-    @BeforeEach
-    public void setup(){
+    @Test
+    public void testHandleInvalidArgumentWith2Error(){
         FieldError fieldError1 = Mockito.mock(FieldError.class);
         when(fieldError1.getField()).thenReturn("field1");
         when(fieldError1.getDefaultMessage()).thenReturn("message1");
@@ -40,12 +47,9 @@ class BooklandExceptionHandlerTest {
         when(bindingResult.getFieldErrors()).thenReturn(fieldErrors);
 
         MethodParameter parameter = Mockito.mock(MethodParameter.class);
-        ex = new MethodArgumentNotValidException(parameter, bindingResult);
-    }
+        methodArgumentNotValidException = new MethodArgumentNotValidException(parameter, bindingResult);
 
-    @Test
-    public void testHandleInvalidArgumentWith2Error(){
-        ResponseEntity<Map<String, String>> resp = handler.handleInvalidArgument(ex);
+        ResponseEntity<Map<String, String>> resp = handler.handleInvalidArgument(methodArgumentNotValidException);
         int status = resp.getStatusCode().value();
         Map<String, String> errors = resp.getBody();
 
@@ -53,5 +57,51 @@ class BooklandExceptionHandlerTest {
         assertNotNull(errors, "Empty error list");
         assertEquals(errors.get("field1"), "message1", "Wrong error message");
         assertEquals(errors.get("field2"), "message2", "Wrong error message");
+    }
+
+    @Test
+    public void testHandleMissingRequestBody(){
+        ResponseEntity<ResponseInfo> resp = handler.handleMissingRequestBody(httpMessageNotReadableException);
+        int status = resp.getStatusCode().value();
+
+        assertEquals(status, HttpStatus.BAD_REQUEST.value(), "Wrong status code");
+        assertNotNull(resp.getBody(), "responseInfo should not be null");
+        assertFalse(resp.getBody().getSuccess(), "Success field should be false");
+    }
+
+    @Test
+    public void testHandleNotMatchException(){
+        NotMatch notMatch = new NotMatch("test message");
+        ResponseEntity<ResponseInfo> resp = handler.handleNotMatchException(notMatch);
+        int status = resp.getStatusCode().value();
+
+        assertEquals(status, HttpStatus.BAD_REQUEST.value(), "Wrong status code");
+        assertNotNull(resp.getBody(), "responseInfo should not be null");
+        assertFalse(resp.getBody().getSuccess(), "Success field should be false");
+        assertEquals(resp.getBody().getMessage(), "test message", "Exception message should be set");
+    }
+
+    @Test
+    public void testHandleAlreadyExistException(){
+        AlreadyExist alreadyExist = new AlreadyExist("test message");
+        ResponseEntity<ResponseInfo> resp = handler.handleAlreadyExistException(alreadyExist);
+        int status = resp.getStatusCode().value();
+
+        assertEquals(status, HttpStatus.BAD_REQUEST.value(), "Wrong status code");
+        assertNotNull(resp.getBody(), "responseInfo should not be null");
+        assertFalse(resp.getBody().getSuccess(), "Success field should be false");
+        assertEquals(resp.getBody().getMessage(), "test message", "Exception message should be set");
+    }
+
+    @Test
+    public void testHandleNotFoundedResource(){
+        NotFound notFound = new NotFound("test message");
+        ResponseEntity<ResponseInfo> resp = handler.handleNotFoundedResource(notFound);
+        int status = resp.getStatusCode().value();
+
+        assertEquals(status, HttpStatus.NOT_FOUND.value(), "Wrong status code");
+        assertNotNull(resp.getBody(), "responseInfo should not be null");
+        assertFalse(resp.getBody().getSuccess(), "Success field should be false");
+        assertEquals(resp.getBody().getMessage(), "test message", "Exception message should be set");
     }
 }
